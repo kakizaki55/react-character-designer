43a6aa46b97dc9092ff11027b0b29d9417d373c2
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-12-02 09:45:23 -0800

    working on the counter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Display from './components/Display/Display';
 import Input from './components/Input/Input';
-// import CounterDisplay from './components/CounterDisplay/CounterDisplay';
+import CounterDisplay from './components/CounterDisplay/CounterDisplay';
 
 function App() {
   const [head, setHead] = useState('');
@@ -10,11 +10,27 @@ function App() {
   const [pants, setPants] = useState('');
   const [phrase, setPhrase] = useState('');
   const [phraseList, setPhraseList] = useState([]);
+  const [headCount, setHeadCount] = useState(0);
+  const [middleCount, setMiddleCount] = useState(0);
+  const [pantCount, setPantCount] = useState(0);
 
   return (
     <div className="App">
       <Display {...{ head, middle, pants, phrase, phraseList }} />
-      <Input {...{ setHead, setMiddle, setPants, setPhrase, phrase, setPhraseList }} />
+      <Input
+        {...{
+          setHead,
+          setMiddle,
+          setPants,
+          setPhrase,
+          phrase,
+          setPhraseList,
+          setHeadCount,
+          setMiddleCount,
+          setPantCount,
+        }}
+      />
+      <CounterDisplay {...{ headCount, middleCount, pantCount }} />
     </div>
   );
 }
--- src/components/CounterDisplay/CounterDisplay.js
+++ src/components/CounterDisplay/CounterDisplay.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-export default function CounterDisplay() {
-  return <></>;
+export default function CounterDisplay(headCount, middleCount, pantCount) {
+  return (
+    <>
+      <div>you have changed the head {headCount} many times</div>
+      <div>you have changed the middle {middleCount} many times</div>
+      <div>you have changed the pants {pantCount} many times</div>
+    </>
+  );
 }
